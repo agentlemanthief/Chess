c9deb420698dff13f6d13be0dee736d18d62700f
Nicholas Baldwin <nicholas.baldwin@icloud.com>
2021-05-02 16:23:50 +0100

    Added king_spec.rb RSpec file with #next_moves test

---
+++ spec/king_spec.rb
@@ -0,0 +1,16 @@
+# Frozen_string_literal: true
+
+# rubocop:disable Metrics/BlockLength
+
+require_relative '../lib/king'
+
+describe 'King' do
+  subject(:king) { King.new([0, 0], 'black') }
+
+  describe '#next_moves' do
+    it 'Returns [[1, 0], [1, 1], [0, 1]] if position is [0, 0]' do
+      next_moves = king.next_moves
+    expect(next_moves).to eq([[1, 0], [1, 1], [0, 1]])
+    end
+  end
+end
