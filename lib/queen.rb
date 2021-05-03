2ecc4571aac07358e721722d20d7eaf3b2a2859e
Nicholas Baldwin <nicholas.baldwin@icloud.com>
2021-05-03 15:13:27 +0100

    Added queen.rb class file

---
+++ lib/queen.rb
@@ -0,0 +1,22 @@
+# Frozen_string_literal: true
+
+require_relative 'chess_piece'
+
+# Class describing a queen chess piece, inheriting from ChessPiece
+class Queen < ChessPiece
+  MOVES = [
+    [1, 1], [2, 2], [3, 3], [4, 4], [5, 5], [7, 7],
+    [1, -1], [2, -2], [3, -3], [4, -4], [5, -5], [7, -7],
+    [-1, 1], [-2, 2], [-3, 3], [-4, 4], [-5, 5], [-7, 7],
+    [-1, -1], [-2, -2], [-3, -3], [-4, -4], [-5, -5], [-7, -7],
+    [1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0],
+    [0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [0, 7],
+    [-1, 0], [-2, 0], [-3, 0], [-4, 0], [-5, 0], [-6, 0], [-7, 0],
+    [0, -1], [0, -2], [0, -3], [0, -4], [0, -5], [0, -6], [0, -7]
+  ].freeze
+
+  def initialize(position, is_white)
+    super(position, is_white)
+    @token = is_white ? "\u2655" : "\u265B"
+  end
+end
